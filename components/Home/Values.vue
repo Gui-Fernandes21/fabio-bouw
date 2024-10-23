ec1c6e1430cd3ddfd29d486f53e3523f12a684d2
GuiFernandes <GuiFernandesPro@gmail.com>
2024-10-22 21:48:41 -0400

    integration with firebase plus new section

---
+++ components/Home/Values.vue
@@ -0,0 +1,19 @@
+
+
+<template>
+
+  <section class="values">
+  
+    <header>
+      <h1>Nos Valeurs</h1>
+    </header>
+
+    <div class="container">
+
+      <img src="/svg/vertical-lines.svg" alt="vertical lines">
+
+    </div>
+
+  </section>
+
+</template>
